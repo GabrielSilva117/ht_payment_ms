99e3f331e07f15919dea98634ac6b8119a0109fe
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2026-02-21 18:08:49 -0300

    feat: Adds the card payment DTOs

--- src/main/java/io/gabrielsilva117/ht_payment_ms/dto/DebitCardPaymentDTO.java
+++ src/main/java/io/gabrielsilva117/ht_payment_ms/dto/DebitCardPaymentDTO.java
@@ -1,4 +1,14 @@
 package io.gabrielsilva117.ht_payment_ms.dto;
 
-public class DebitCardPaymentDTO {
+import io.gabrielsilva117.ht_payment_ms.enums.PaymentType;
+
+import java.math.BigDecimal;
+
+public class DebitCardPaymentDTO extends CardPaymentDTO {
+    private BigDecimal dailyLimit;
+
+
+    public DebitCardPaymentDTO () {
+        this.setType(PaymentType.CARD_DEBIT);
+    }
 }
