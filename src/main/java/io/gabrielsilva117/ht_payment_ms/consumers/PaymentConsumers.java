6f905d96947e61edbe1627859a0509680be1cf54
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2026-03-07 17:34:33 -0300

    feat: Adds ack and nack method to the consumer service method
    Also adds new parameters and a error handling method

--- src/main/java/io/gabrielsilva117/ht_payment_ms/consumers/PaymentConsumers.java
+++ src/main/java/io/gabrielsilva117/ht_payment_ms/consumers/PaymentConsumers.java
@@ -1,8 +1,10 @@
 package io.gabrielsilva117.ht_payment_ms.consumers;
 
+import com.rabbitmq.client.Channel;
 import io.gabrielsilva117.ht_payment_ms.config.MQConfig;
 import io.gabrielsilva117.ht_payment_ms.service.PaymentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -12,9 +14,9 @@ public class PaymentConsumers {
     private final PaymentService paymentService;
 
     @RabbitListener(queues = MQConfig.PAYMENT_QUEUE)
-    public void handlePayments(String message) {
+    public void handlePayments(String payload, Channel channel, Message msg) {
         try {
-            this.paymentService.processPayment(message);
+            this.paymentService.processPayment(payload, channel, msg);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
